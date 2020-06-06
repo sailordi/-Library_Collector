158a6be7c5a2395b1e916e798c0c659fa8283e57
Sailordi <sailordi11@gmail.com>
2020-06-06 17:37:04 +0300

    Created the project

---
+++ WindowMain.h
@@ -0,0 +1,34 @@
+#ifndef WINDOWMAIN_H
+#define WINDOWMAIN_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class WindowMain; }
+QT_END_NAMESPACE
+
+/*! \class WindowMain
+ * \brief The class is the main ui for the ptogram
+ *
+ * Incudes: QMainWindow
+ *
+ * Inherits: QMainWindow
+ *
+ * \author Created by: Sailordi
+ * \author Last to touch it: Sailordi
+ *
+ * \date Created: 2020-06-06
+ * \date Last update: 2020-06-06
+*/
+class WindowMain : public QMainWindow
+{
+    Q_OBJECT
+public:
+    WindowMain(QWidget *parent = nullptr);
+    ~WindowMain();
+
+private:
+    Ui::WindowMain* v_ui = nullptr;
+
+};
+#endif // WINDOWMAIN_H
